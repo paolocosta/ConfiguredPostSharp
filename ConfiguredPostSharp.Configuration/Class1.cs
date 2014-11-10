02a2d8e8fe731e0181461c444f95995bcc01a73f
paolocosta <montella>
2014-11-10 21:59:29 +0100

    3

---
+++ ConfiguredPostSharp.Configuration/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfiguredPostSharp.Configuration
+{
+    public class Class1
+    {
+        [Exception]
+        [MethodBoundary]
+        public string Test<T>(string p1, int p2)
+        {
+            return 1.ToString();
+        }
+    }
+}
